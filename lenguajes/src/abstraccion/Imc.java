fdb2b6198941e3f653b7b6e04f19ed5dcec9054f
T-102 <T-102@PC259>
2017-09-18 19:40:02 -0500

    Ya acabe :)

--- lenguajes/src/abstraccion/Imc.java
+++ lenguajes/src/abstraccion/Imc.java
@@ -3,10 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+
 package abstraccion;
 
 
 public class Imc {
     Usuario u;
     String resultado;
+    
+    public String calcular(){
+        float mivalor=u.peso/(u.altura*u.altura);
+        resultado="Tu imc es "+mivalor;
+        return resultado;
+    
+    }
 }
