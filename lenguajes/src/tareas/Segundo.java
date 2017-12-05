8204d7d250afa8cf30a63bb7fda643069b76656d
T-102 <T-102@PC245>
2017-12-04 19:41:48 -0600

    4/12/17 threads Marisela Garcia

---
+++ lenguajes/src/tareas/Segundo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareas;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author T-102
+ */
+public class Segundo extends Thread{
+    public void run(){
+        while(true){
+            try {
+                //aqui pones la accion de tu Thread
+                System.out.println("soy el segundo Thread que no hace nada");
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Segundo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+        }
+    }
+    
+}
